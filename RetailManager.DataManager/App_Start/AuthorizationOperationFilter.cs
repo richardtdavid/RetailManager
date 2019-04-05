d8e10e9fc5b67058eaf07cc02a17ccad82bccb3b
Richard David <driichard@gmail.com>
2019-04-04 20:58:05 -0500

    Add swagger documentation to API
    Add Swashbuckle/Swagger to API for document and testing

---
+++ RetailManager.DataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,32 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+//https://stackoverflow.com/questions/51117655/how-to-use-swagger-in-asp-net-webapi-2-0-with-token-based-authentication
+
+namespace RetailManager.DataManager.App_Start
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if(operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access token",
+                required = false,
+                type = "string"
+            });
+
+        }
+    }
+}
